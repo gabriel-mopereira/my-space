17bdc1820b0c99a1446ac1306a6275a334dc918c
Gabriel <gabrielmoreira@protonmail.com>
2026-04-21 14:03:38 -0300

    Scroll to center of page on mobile instead of CSS offset

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,32 +1,42 @@
+"use client";
+
+import { useEffect } from "react";
 import Grainient from "@/components/Grainient";
 
-const Background = () => (
-  <div className="absolute w-full h-full">
-    <Grainient
-      color1="#8b7bb8"
-      color2="#a2a0ca"
-      color3="#6b7ec2"
-      timeSpeed={1}
-      colorBalance={0}
-      warpStrength={1}
-      warpFrequency={8.5}
-      warpSpeed={4}
-      warpAmplitude={40}
-      blendAngle={0}
-      blendSoftness={0.1}
-      rotationAmount={500}
-      noiseScale={2}
-      grainAmount={0.1}
-      grainScale={2}
-      grainAnimated={false}
-      contrast={1.5}
-      gamma={1}
-      saturation={1}
-      centerX={0}
-      centerY={0}
-      zoom={0.9}
-    />
-  </div>
-);
+const Background = () => {
+  useEffect(() => {
+    document.documentElement.scrollTop =
+      (document.documentElement.scrollHeight - window.innerHeight) / 2;
+  }, []);
+
+  return (
+    <div className="absolute w-full h-full">
+      <Grainient
+        color1="#8b7bb8"
+        color2="#a2a0ca"
+        color3="#6b7ec2"
+        timeSpeed={1}
+        colorBalance={0}
+        warpStrength={1}
+        warpFrequency={8.5}
+        warpSpeed={4}
+        warpAmplitude={40}
+        blendAngle={0}
+        blendSoftness={0.1}
+        rotationAmount={500}
+        noiseScale={2}
+        grainAmount={0.1}
+        grainScale={2}
+        grainAnimated={false}
+        contrast={1.5}
+        gamma={1}
+        saturation={1}
+        centerX={0}
+        centerY={0}
+        zoom={0.9}
+      />
+    </div>
+  );
+};
 
 export default Background;
