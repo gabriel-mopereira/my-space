ab8c5c9ebb57a4304798ccaa0844a86f64fea13f
Gabriel <gabrielmoreira@protonmail.com>
2026-04-28 19:26:07 -0300

    Replace Grainient background with DitherWave effect

--- src/components/effects/background.tsx
+++ src/components/effects/background.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect } from "react";
-import Grainient from "@/components/effects/grainient";
+import DitherWave from "./dither-wave";
 
 const Background = () => {
   useEffect(() => {
@@ -11,29 +11,16 @@ const Background = () => {
 
   return (
     <div className="absolute w-full h-full">
-      <Grainient
-        blendAngle={0}
-        blendSoftness={0.1}
-        centerX={0}
-        centerY={0}
-        color1="#8b7bb8"
-        color2="#a2a0ca"
-        color3="#6b7ec2"
-        colorBalance={0}
-        contrast={1.5}
-        gamma={1}
-        grainAmount={0.1}
-        grainAnimated={false}
-        grainScale={2}
-        noiseScale={2}
-        rotationAmount={500}
-        saturation={1}
-        timeSpeed={1}
-        warpAmplitude={40}
-        warpFrequency={8.5}
-        warpSpeed={4}
-        warpStrength={1}
-        zoom={0.9}
+      <DitherWave
+        downScale={0.5}
+        intensity={1}
+        primaryColor="#8b7bb8"
+        quality="low"
+        scale={3}
+        secondaryColor="#a2a0ca"
+        speed={0.2}
+        tertiaryColor="#6b7ec2"
+        width="100%"
       />
     </div>
   );
